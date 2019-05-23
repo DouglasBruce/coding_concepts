59d55a00a51121cf0e0fdd74355c7fd92ab19c67
DouglasBruce <douglasbruce96@gmail.com>
2019-05-23 11:09:54 +0100

    Added left, right and backwards movement

--- src/DirectionControls.js
+++ src/DirectionControls.js
@@ -3,6 +3,18 @@ import left_arrow from './images/left_arrow.svg';
 
 class DirectionControls extends Component{
 
+    backClick(e) {
+      var character = this.props.characterRef.current;
+      character.moveBack();
+      debugger;
+    }
+
+    leftClick(e) {
+      var character = this.props.characterRef.current;
+      character.moveLeft();
+      debugger;
+    }
+
     forwardClick(e){
       //instance of Character.js injected as property during instantiation
       var character = this.props.characterRef.current;
@@ -10,15 +22,21 @@ class DirectionControls extends Component{
       debugger;
     }
 
+    rightClick(e) {
+      var character = this.props.characterRef.current;
+      character.moveRight();
+      debugger;
+    }
+
     render(){
         return(
             <div id="directionControls" class="columnLayout">
               <div class="rowLayout">
-                <img src={left_arrow} class="arrow backArrow"/>
+                <img src={left_arrow} class="arrow backArrow" onClick={(e) => this.backClick(e)} />
                 <div class="columnLayout">
-                  <img src={left_arrow} class="arrow"/>
+                  <img src={left_arrow} class="arrow" onClick={(e) => this.leftClick(e)} />
                   <img src={left_arrow} class="arrow forwardArrow" onClick={(e) => this.forwardClick(e)} />
-                  <img src={left_arrow} class="arrow rightArrow" />
+                  <img src={left_arrow} class="arrow rightArrow" onClick={(e) => this.rightClick(e)} />
                 </div>
               </div>
             </div>
